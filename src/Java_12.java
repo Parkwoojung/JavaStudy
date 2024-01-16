7cb88d8b02cd1a1cfac3bce0444efb29bef0bc69
Uddoro <lowell0322@naver.com>
2024-01-16 20:28:25 +0900

    240116 백준

---
+++ src/Java_12.java
@@ -0,0 +1,8 @@
+public class Java_12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                            " )  ( ')\n" +
+                            "(  /  )\n" +
+                            " \\(__)|");
+    }
+}
---
+++ src/Java_13.java
@@ -0,0 +1,9 @@
+public class Java_13 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                           "|q p|   /}\n" +
+                           "( 0 )\"\"\"\\\n" +
+                           "|\"^\"`    |\n" +
+                           "||_/=\\\\__|");
+    }
+}
